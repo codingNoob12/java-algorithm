a84bb9c3971379b7501463f214edcc64436b5ee3
codingNoob12 <ilhyeon0326@gmail.com>
2025-06-19 13:30:38 +0000

    [LeetCode 344] Reverse String 추가 * 배열 스왑 방식

---
+++ src/solution/leetcode/string/reversestring/Solution1.java
@@ -0,0 +1,14 @@
+package solution.leetcode.string.reversestring;
+
+public class Solution1 {
+    public void reverseString(char[] s) {
+        int left = 0, right = s.length - 1;
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
