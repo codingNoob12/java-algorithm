7e5078ccb6d1a8e2aa87246832a33cbe4bd07b37
codingNoob12 <ilhyeon0326@gmail.com>
2025-06-24 13:43:52 +0000

    [LeetCode 121] Best Time to Buy and Sell Stock 추가 ## 최저가와 현재가의 차이를 이용한 풀이 먼저, prices의 길이는 최대 10^5이므로 O(N^2)인 브루트포스로는 시간 초과가 발생한다. 그러므로, 다른 풀이를 생각해보자. 가장 단순하게 생각해보면, 주식은 저점에서 사서 고점에 팔면 이득을 볼 것이다. 그렇다고 무작정 prices 배열의 최솟값과 최댓값의 차이를 maxProfit으로 생각하면 안된다. 최솟값이 최댓값 이후에 존재한다면, 우리는 먼저 주식을 팔고 나중에 주식을 사는 상황이 된다. 하지만, 이는 불가능하다. 문제 조건에서 판매일은 구매일 이후라고 나와있기 때문이다. 따라서, 단순히 최솟값과 최댓값의 차이로 접근하는 방법은 굉장히 위험한 방법이다. 그러므로, 우리는 i번째날에 무조건 판다고 가정하면, 그 이전에 존재하는 최소 가격으로 주식을 샀을 것이다. 따라서, i번째 날 까지의 주식의 최소 가격을 저장할 변수 minPrice를 도입하고, 현재가와의 차이를 이용해서 maxProfit을 구하면 된다.

---
+++ src/solution/leetcode/array/besttimetobuyandsellstock/Solution1.java
@@ -0,0 +1,13 @@
+package solution.leetcode.array.besttimetobuyandsellstock;
+
+public class Solution1 {
+    public int maxProfit(int[] prices) {
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        for (int price : prices) {
+            minPrice = Math.min(minPrice, price);
+            maxProfit = Math.max(maxProfit, price - minPrice);
+        }
+        return maxProfit;
+    }
+}
